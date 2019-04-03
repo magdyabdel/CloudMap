4856513dfde4090389a912bbe88902fd369c4e5a
Magdy Abdel <magdy.abdel@student.kuleuven.be>
2019-04-03 22:30:16 +0200

    Cleaned up code, added comments and updated README

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 CloudMap is a python project for EE5. It converts GeoTIFF files into images where a route (based on an Excel file) and 
 the current location is plotted on.
 
-## Test
-To test the code you can use the _environment.yml_ file to create the python environment with Anaconda. Don't forget to 
-run the script as follows:
+## Usage
+To generate the exact environment used for this project one can find the _environment.yml_ file to create the python 
+environment with Anaconda. Following packages are included:
+- Basemap
+- GDAL
+- NumPy
+- PyPlot
+- Pandas
+
+The script has to be executed as follows: 
 `python geotiff_to_map.py [GeoTIFF_file] [Excel_file]`
-Where the GeoTIFF file can be _test.tif_ and the Excel file can be _latlon.xlsx_.
+
+Note: For testing purposes the GeoTIFF file can be _test.tif_ and the Excel file can be _latlon.xlsx_ which are provided
+inside this project.
--- geotiff_to_map.py
+++ geotiff_to_map.py
@@ -1,101 +1,127 @@
 # -*- coding: utf-8 -*-
 from mpl_toolkits.basemap import Basemap
-import osr, gdal
+import osr
+import gdal
 import numpy as np
 from sys import argv
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+def convert_xy_projection(source_xy, source_projection, convert_projection):
+    """
+    Function to convert GeoTIFF file coordinates with origin_proj as projection into convert_projection projection.
+    :param source_xy: Original XY coordinates from GeoTIFF file
+    :param source_projection: Projection of original XY coordinates from GeoTIFF file
+    :param convert_projection: Desired projection to convert original XY coordinates into
+    :return: x_out, y_out - Two mxn matrices with converted projection data
+    """
+
+    # Return the dimensions of source_xy
+    shape = source_xy[0, :, :].shape
+    # Return total number of elements in source_xy
+    size = source_xy[0, :, :].size
+
+    # Create a CoordinateTransformation object with specified source_projection and convert_projection
+    ct = osr.CoordinateTransformation(source_projection, convert_projection)
+
+    # Reshape: Reshape the source_xy to a mxn (bm=2, n=size) matrix and transpose it
+    # TransformPoints: Convert the reshaped matrix's coordinates to the desired convert_projection as
+    # defined by the ct object
+    # Array: Create an array from transformed points with first column x
+    xy_target = np.array(ct.TransformPoints(source_xy.reshape(2, size).T))
+
+    # Reshape the xy_target from a one dimensional matrix to a matrix of mxn dimensions x_out and y_out
+    x_out = xy_target[:, 0].reshape(shape)
+    y_out = xy_target[:, 1].reshape(shape)
+
+    return x_out, y_out
+
+
+# Return the console arguments in a variable
+# Usage: filetoread[n]
 filetoread = argv
 
+# Read GeoTIFF data into a GDAL object (dataset)
+geo_dataset = gdal.Open(filetoread[1])
 
-def convertXY(xy_source, inproj, outproj):
-    # function to convert coordinates
+# Reading GDAL dataset as array to be able to plot it afterwards
+data = geo_dataset.ReadAsArray()
 
-    shape = xy_source[0, :, :].shape
-    size = xy_source[0, :, :].size
+# Read in GeoTransform data where (in a north up image without any rotation or shearing):
+# GeoTransform[0] = top left x coordinate (longtitude)
+# GeoTransform[1] = pixel width (w-e) [degree/pixel]
+# GeoTransform[2] = 0
+# GeoTransform[3] = top left y coordinate (latitude)
+# GeoTransform[4] = 0
+# GeoTransform[5] = pixel height (n-s) (negative value) [degree/pixel]
+gt = geo_dataset.GetGeoTransform()
+# Read projection from GDAL object
+proj = geo_dataset.GetProjection()
 
-    # the ct object takes and returns pairs of x,y, not 2d grids
-    # so the the grid needs to be reshaped (flattened) and back.
-    ct = osr.CoordinateTransformation(inproj, outproj)
-    xy_target = np.array(ct.TransformPoints(xy_source.reshape(2, size).T))
+x_resolution = gt[1]
+y_resolution = gt[5]
 
-    xx = xy_target[:, 0].reshape(shape)
-    yy = xy_target[:, 1].reshape(shape)
+# Return minimum and maximum coordinates of the GeoTIFF image
+# Note: RasterXSize and RasterYSize return the raster width and height in pixels
+# Note: Adding or removing half the resolution is a small correction
+xmin = gt[0] + x_resolution * 0.5
+xmax = gt[0] + (x_resolution * geo_dataset.RasterXSize) - x_resolution * 0.5
+ymin = gt[3] + (y_resolution * geo_dataset.RasterYSize) + y_resolution * 0.5
+ymax = gt[3] - y_resolution * 0.5
 
-    return xx, yy
+# Clear dataset
+geo_dataset = None
 
+# Create a dense meshgrid from coordindates from the original dataset
+original_xy = np.mgrid[xmin:xmax + x_resolution:x_resolution, ymax + y_resolution:ymin:y_resolution]
 
-# Read the data and metadata
-ds = gdal.Open(filetoread[1])
-
-data = ds.ReadAsArray()
-gt = ds.GetGeoTransform()
-proj = ds.GetProjection()
-
-xres = gt[1]
-yres = gt[5]
-
-# get the edge coordinates and add half the resolution
-# to go to center coordinates
-xmin = gt[0] + xres * 0.5
-xmax = gt[0] + (xres * ds.RasterXSize) - xres * 0.5
-ymin = gt[3] + (yres * ds.RasterYSize) + yres * 0.5
-ymax = gt[3] - yres * 0.5
-
-print('min X = ', xmin)
-print('max X = ', xmax)
-print('min Y = ', ymin)
-print('max Y = ', ymax)
-
-ds = None
-
-# create a grid of xy coordinates in the original projection
-xy_source = np.mgrid[xmin:xmax+xres:xres, ymax+yres:ymin:yres]
-print("figure")
-# Create the figure and basemap object
+# Create the figure (figsize is given in inches)
 fig = plt.figure(figsize=(20, 20))
-# m = Basemap(projection='ortho', lon_0=xmin+(xmax-xmin)/2, lat_0=ymin+(ymax-ymin)/2, resolution='l')
-m = Basemap(projection='ortho', lon_0=141.17, lat_0=2.177, resolution='l')
 
-# Create the projection objects for the convertion
-# original (Albers)
-inproj = osr.SpatialReference()
-inproj.ImportFromWkt(proj)
+# Create basemap object
+# Note: We center the orthogonala projection with lon_0 and lat_0
+bm = Basemap(projection='ortho', lon_0=xmin+(xmax-xmin)/2, lat_0=ymin+(ymax-ymin)/2, resolution='l')
 
-# Get the target projection from the basemap object
-outproj = osr.SpatialReference()
-outproj.ImportFromProj4(m.proj4string)
+# Create the projection objects for the conversion
+# Get original projection
+origin_proj = osr.SpatialReference()
+origin_proj.ImportFromWkt(proj)
+# Get target projection from Basemap object (projection argument)
+target_proj = osr.SpatialReference()
+target_proj.ImportFromProj4(bm.proj4string)
 
-print("Convert XY")
-# Convert from source projection to basemap projection
-xx, yy = convertXY(xy_source, inproj, outproj)
-print("Plotting data")
-# plot the data (first layer)
-im1 = m.pcolormesh(xx, yy, data[0, :, :].T, cmap=plt.cm.gray)
-# im1 = m.contourf(xx, yy, data[0, :, :].T, 20, cmap=plt.cm.gray)
+# Convert coordinates original_xy from original projection to target projection
+converted_x, converted_y = convert_xy_projection(original_xy, origin_proj, target_proj)
 
-# annotate
-m.drawcoastlines(linewidth=.1)
+# Plot data at coordinates converted_x and converted_y using a color map
+bm.pcolormesh(converted_x, converted_y, data[0, :, :].T, cmap=plt.cm.gray)
 
-print("Reading excel")
+# Draw coastlines on map
+bm.drawcoastlines(linewidth=.1)
 
-# Reading excel
+# Reading excel from file and select sheet
 df = pd.read_excel(filetoread[2], sheet_name='Sheet1')
 
+# Retrieve dataframe columns latitude and lontitude
 listLat = df['latitude']
 listLon = df['longitude']
 
-xlon, ylat = m(listLon.tolist(), listLat.tolist())
+# Using basemap instance to convert lontitude and lattitude to position on the map
+xlon, ylat = bm(listLon.tolist(), listLat.tolist())
 
-plt.scatter(xlon, ylat, zorder=10, s=0.1, alpha=1, marker='.', color='Red')
+# Plot the route as dots on the map
+plt.scatter(xlon, ylat, zorder=10, s=0.1, alpha=1, marker=',', color='Red')
 
+# TODO: Retrieve current location (currently constant values on route)
 xlon_me = 134.1791
 ylat_me = -28.36473
 
-xlon_me_map, ylat_me_map = m(xlon_me, ylat_me)
-m.plot(xlon_me_map, ylat_me_map, zorder=15, marker='.', alpha=1, markersize=0.1, color='Blue')
+# Using basemap instance to convert lontitude and lattitude to position on the map
+xlon_me_map, ylat_me_map = bm(xlon_me, ylat_me)
 
+# Plot the current location as a dot on the map
+bm.plot(xlon_me_map, ylat_me_map, zorder=30, marker='.', alpha=1, markersize=0.1, color='Yellow')
+
+# Saving an image from the generated file
 plt.savefig('world.png', dpi=300)
-
-print('saved')
